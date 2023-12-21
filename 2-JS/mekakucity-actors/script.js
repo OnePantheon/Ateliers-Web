f967c8e9b0cc36790244c676af076c43972062c6
LoganTann <logon313@hotmail.fr>
2023-12-21 19:09:35 +0100

    hotfixes TP2

--- 2-JS/mekakucity-actors/script.js
+++ 2-JS/mekakucity-actors/script.js
@@ -1,17 +1,17 @@
 
 const episodes = [
-    "http://archive.kagescan.fr/anime/mekakucity-actors/mekakucity_actors_01_vostfr.mp4",
-    "http://archive.kagescan.fr/anime/mekakucity-actors/mekakucity_actors_02_vostfr.mp4",
-    "http://archive.kagescan.fr/anime/mekakucity-actors/mekakucity_actors_03_vostfr.mp4",
-    "http://archive.kagescan.fr/anime/mekakucity-actors/mekakucity_actors_04_vostfr.mp4",
-    "http://archive.kagescan.fr/anime/mekakucity-actors/mekakucity_actors_05_vostfr.mp4",
-    "http://archive.kagescan.fr/anime/mekakucity-actors/mekakucity_actors_06_vostfr.mp4",
-    "http://archive.kagescan.fr/anime/mekakucity-actors/mekakucity_actors_07_vostfr.mp4",
-    "http://archive.kagescan.fr/anime/mekakucity-actors/mekakucity_actors_08_vostfr.mp4",
-    "http://archive.kagescan.fr/anime/mekakucity-actors/mekakucity_actors_09_vostfr.mp4",
-    "http://archive.kagescan.fr/anime/mekakucity-actors/mekakucity_actors_10_vostfr.mp4",
-    "http://archive.kagescan.fr/anime/mekakucity-actors/mekakucity_actors_11_vostfr.mp4",
-    "http://archive.kagescan.fr/anime/mekakucity-actors/mekakucity_actors_12_vostfr.mp4",
+    "https://archive.kagescan.fr/anime/mekakucity-actors/mekakucity_actors_01_vostfr.mp4",
+    "https://archive.kagescan.fr/anime/mekakucity-actors/mekakucity_actors_02_vostfr.mp4",
+    "https://archive.kagescan.fr/anime/mekakucity-actors/mekakucity_actors_03_vostfr.mp4",
+    "https://archive.kagescan.fr/anime/mekakucity-actors/mekakucity_actors_04_vostfr.mp4",
+    "https://archive.kagescan.fr/anime/mekakucity-actors/mekakucity_actors_05_vostfr.mp4",
+    "https://archive.kagescan.fr/anime/mekakucity-actors/mekakucity_actors_06_vostfr.mp4",
+    "https://archive.kagescan.fr/anime/mekakucity-actors/mekakucity_actors_07_vostfr.mp4",
+    "https://archive.kagescan.fr/anime/mekakucity-actors/mekakucity_actors_08_vostfr.mp4",
+    "https://archive.kagescan.fr/anime/mekakucity-actors/mekakucity_actors_09_vostfr.mp4",
+    "https://archive.kagescan.fr/anime/mekakucity-actors/mekakucity_actors_10_vostfr.mp4",
+    "https://archive.kagescan.fr/anime/mekakucity-actors/mekakucity_actors_11_vostfr.mp4",
+    "https://archive.kagescan.fr/anime/mekakucity-actors/mekakucity_actors_12_vostfr.mp4",
 ];
 
 /*
@@ -28,6 +28,20 @@ function setChapter(episodeNumber) {
     span.innerText = String(episodeNumber);
 }
 
+function updateBtnStates(episodeNumber) {
+    const prevBtn = document.querySelector("#prev");
+    const nextBtn = document.querySelector("#next");
+
+    prevBtn.classList.remove("disabled");
+    nextBtn.classList.remove("disabled");
+
+    if (episodeNumber === 1) {
+        prevBtn.classList.add("disabled");
+    } else if (episodeNumber === episodes.length) {
+        nextBtn.classList.add("disabled");
+    }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     let episodeNumber = 1;
     const prevBtn = document.querySelector("#prev");
@@ -37,19 +51,14 @@ document.addEventListener("DOMContentLoaded", () => {
         if (episodeNumber > 1) {
             episodeNumber--;
             setChapter(episodeNumber);
-            prevBtn.classList.add("remove");
-        } else {
-            prevBtn.classList.add("disabled");
         }
     });
     nextBtn.addEventListener("click", () => {
         if (episodeNumber < episodes.length) {
             episodeNumber++;
             setChapter(episodeNumber);
-            prevBtn.classList.add("remove");
-        } else {
-            nextBtn.classList.add("disabled");
         }
+        updateBtnStates(episodeNumber);
     });
 
 });
